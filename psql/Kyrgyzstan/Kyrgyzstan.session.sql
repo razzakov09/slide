c3bd86c39162130e20c911de78777f22389403e5
avtandil <avtandilkgg@gmail.com>
2022-04-28 19:08:57 +0600

    Done Kyrgyzstan db taks

---
+++ psql/Kyrgyzstan/Kyrgyzstan.session.sql
@@ -0,0 +1,33 @@
+-- #1
+-- CREATE DATABASE kyrgyzstan;
+
+-- #2
+-- CREATE TABLE developers(
+-- id SERIAL PRIMARY KEY,
+-- name VARCHAR,
+-- population INTEGER);
+
+-- INSERT INTO developers(
+--     name,
+--     partticipants
+-- ) VALUES 
+-- ('Chui', 100000),
+-- ('Osh', 200000),
+-- ('Naryn', 300000);
+
+-- #3
+-- ALTER TABLE developers
+-- ADD teams VARCHAR;
+
+-- #4
+-- ALTER TABLE developers 
+-- RENAME COLUMN population TO partticipants;
+
+-- #5
+-- DELETE FROM developers
+-- WHERE partticipants = 300000;
+
+-- #6
+-- UPDATE developers
+-- SET partticipants = '7000'
+-- WHERE partticipants > '8000';
